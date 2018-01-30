149547c55413fa2a20efc86f02b8a55f2ba8d526
Finch <finch@play-net.org>
2018-01-30 09:56:52 +0100

    Add vote undo feature

--- cmd/democracy.bot/bot.go
+++ cmd/democracy.bot/bot.go
@@ -130,6 +130,7 @@ func do(log *zap.Logger) error {
 	log.Info("adding handlers")
 	discord.AddHandler(bot.ready)
 	discord.AddHandler(bot.messageCreate)
+	discord.AddHandler(bot.reactionAdd)
 
 	err = discord.Open()
 	if err != nil {
@@ -150,71 +151,6 @@ func (b *bot) ready(s *discordgo.Session, event *discordgo.Ready) {
 	s.UpdateStatus(0, "democracy")
 	s.State.TrackChannels = true
 	s.State.MaxMessageCount = 100
-
-}
-
-func (b *bot) messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-
-	s.State.TrackChannels = true
-	s.State.MaxMessageCount = 100
-
-	if strings.HasPrefix(m.Content, "!democracy") {
-		m.Content = strings.TrimPrefix(m.Content, "!democracy ")
-		b.log.Info("handling message", zap.String("channel", m.ChannelID), zap.String("msg", m.Content))
-
-		c, err := s.State.Channel(m.ChannelID)
-		if err != nil {
-			b.log.Error("could not find channel")
-			return
-		}
-
-		if b.demoChan == nil {
-			guild, err := s.Guild(c.GuildID)
-			if err != nil {
-				b.log.Error("could not fetch democracy channel", zap.String("guild", c.GuildID), zap.Error(err))
-				return
-			}
-			for _, c := range guild.Channels {
-				if c.Name == "democracy" {
-					b.demoChan = c
-				}
-			}
-		}
-
-		if m.Content == "init" {
-			b.init(s, m, c)
-			s.ChannelMessageDelete(c.ID, m.ID)
-		}
-
-		if strings.HasPrefix(m.Content, "vote") {
-			err = b.vote(s, m, c)
-			s.ChannelMessageDelete(c.ID, m.ID)
-			var feedbackEmbed *discordgo.Message
-			if err == nil {
-				feedbackEmbed, err = s.ChannelMessageSendEmbed(c.ID, &discordgo.MessageEmbed{
-					Title:       "Vote created",
-					Description: "Press :leftwards_arrow_with_hook: to Undo",
-				})
-				err = s.MessageReactionAdd(c.ID, feedbackEmbed.ID, "↩")
-				if err != nil {
-					b.log.Error("unable to add emoji", zap.Error(err))
-					s.ChannelMessageDelete(c.ID, feedbackEmbed.ID)
-				}
-				return
-			}
-
-			b.log.Error("unable to send embed", zap.Error(err))
-			feedbackEmbed, err = s.ChannelMessageSendEmbed(c.ID, &discordgo.MessageEmbed{
-				Title:       "Vote failed",
-				Description: fmt.Sprintf("Error: %s", err.Error()),
-			})
-			return
-
-		}
-	}
 }
 
 func (b *bot) init(s *discordgo.Session, m *discordgo.MessageCreate, c *discordgo.Channel) bool {
@@ -280,16 +216,16 @@ func (b *bot) init(s *discordgo.Session, m *discordgo.MessageCreate, c *discordg
 				Value:  "!democracy vote [title]|[text]",
 				Inline: true,
 			},
-			&discordgo.MessageEmbedField{
-				Name:   "Start Distrust Vote",
-				Value:  "!democracy distrust [reason]",
-				Inline: true,
-			},
-			&discordgo.MessageEmbedField{
-				Name:   "Suggest new Admin",
-				Value:  "To do so, text the bot in private with '!democracy admin [user]'.",
-				Inline: false,
-			},
+			//&discordgo.MessageEmbedField{
+			//	Name:   "Start Distrust Vote",
+			//	Value:  "!democracy distrust [reason]",
+			//	Inline: true,
+			//},
+			//&discordgo.MessageEmbedField{
+			//	Name:   "Suggest new Admin",
+			//	Value:  "To do so, text the bot in private with '!democracy admin [user]'.",
+			//	Inline: false,
+			//},
 		},
 	}
 
@@ -299,6 +235,105 @@ func (b *bot) init(s *discordgo.Session, m *discordgo.MessageCreate, c *discordg
 		return false
 	}
 	return true
+}
+
+func (b *bot) messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	s.State.TrackChannels = true
+	s.State.MaxMessageCount = 100
+
+	if strings.HasPrefix(m.Content, "!democracy") {
+		m.Content = strings.TrimPrefix(m.Content, "!democracy ")
+		b.log.Info("handling message", zap.String("channel", m.ChannelID), zap.String("msg", m.Content))
+
+		c, err := s.State.Channel(m.ChannelID)
+		if err != nil {
+			b.log.Error("could not find channel")
+			return
+		}
+
+		if b.demoChan == nil {
+			guild, err := s.Guild(c.GuildID)
+			if err != nil {
+				b.log.Error("could not fetch democracy channel", zap.String("guild", c.GuildID), zap.Error(err))
+				return
+			}
+			for _, c := range guild.Channels {
+				if c.Name == "democracy" {
+					b.demoChan = c
+				}
+			}
+		}
+
+		if m.Content == "init" {
+			b.init(s, m, c)
+			s.ChannelMessageDelete(c.ID, m.ID)
+		}
+
+		if strings.HasPrefix(m.Content, "vote") {
+			err = b.vote(s, m, c)
+			s.ChannelMessageDelete(c.ID, m.ID)
+			if err != nil {
+				b.log.Error("unable to send embed", zap.Error(err))
+				s.ChannelMessageSendEmbed(c.ID, &discordgo.MessageEmbed{
+					Title:       "Vote failed",
+					Description: fmt.Sprintf("Error: %s", err.Error()),
+				})
+				return
+			}
+		}
+	}
+}
+
+func (b *bot) reactionAdd(s *discordgo.Session, m *discordgo.MessageReactionAdd) {
+	if m.UserID == s.State.User.ID {
+		return
+	}
+
+	b.log.Info("reaction event",
+		zap.String("user", m.UserID),
+		zap.String("message", m.MessageID),
+		zap.String("channel", m.ChannelID),
+		zap.String("emoji", m.Emoji.Name),
+	)
+
+	if m.Emoji.Name == "↩" {
+		msg, err := s.ChannelMessage(m.ChannelID, m.MessageID)
+		if err != nil {
+			b.log.Error("unable to find message", zap.Error(err))
+			return
+		}
+		if len(msg.Embeds) < 1 {
+			b.log.Error("unable to find message embed", zap.Error(err))
+			return
+		}
+		embed := msg.Embeds[0]
+		if embed.Author.Name != m.UserID {
+			b.log.Info("permission denied for undo", zap.String("expected", embed.Author.Name), zap.String("user", m.UserID))
+			return
+		}
+		if embed.Title != "Vote created" {
+			b.log.Info("not a vote create event")
+			return
+		}
+		voteID := embed.Description
+		err = s.ChannelMessageDelete(b.demoChan.ID, voteID)
+		if err != nil {
+			b.log.Error("unable to delete vote", zap.Error(err))
+			return
+		}
+		embed.Title = "Vote deleted"
+		embed.Description = voteID
+		embed.Fields = []*discordgo.MessageEmbedField{}
+		undoMsg := discordgo.NewMessageEdit(m.ChannelID, m.MessageID)
+		undoMsg.Embed = embed
+		//s.ChannelMessageDelete(m.ChannelID, m.MessageID)
+		s.ChannelMessageEditComplex(undoMsg)
+		s.MessageReactionsRemoveAll(m.ChannelID, m.MessageID)
+	}
 }
 
 func (b *bot) vote(s *discordgo.Session, m *discordgo.MessageCreate, c *discordgo.Channel) error {
@@ -351,6 +386,32 @@ func (b *bot) vote(s *discordgo.Session, m *discordgo.MessageCreate, c *discordg
 	if err != nil {
 		b.log.Error("unable to add emoji", zap.Error(err))
 		s.ChannelMessageDelete(b.demoChan.ID, voteEmbed.ID)
+		return err
+	}
+	feedbackEmbed, err := s.ChannelMessageSendEmbed(c.ID, &discordgo.MessageEmbed{
+		Title:       "Vote created",
+		Description: voteEmbed.ID,
+		Author: &discordgo.MessageEmbedAuthor{
+			Name:    m.Author.ID,
+			IconURL: m.Author.AvatarURL("100x100"),
+		},
+		Fields: []*discordgo.MessageEmbedField{
+			&discordgo.MessageEmbedField{
+				Name:   "User",
+				Value:  m.Author.Username,
+				Inline: true,
+			},
+			&discordgo.MessageEmbedField{
+				Name:   "Undo",
+				Value:  "Press :leftwards_arrow_with_hook:",
+				Inline: true,
+			},
+		},
+	})
+	err = s.MessageReactionAdd(c.ID, feedbackEmbed.ID, "↩")
+	if err != nil {
+		b.log.Error("unable to add emoji", zap.Error(err))
+		s.ChannelMessageDelete(c.ID, feedbackEmbed.ID)
 		return err
 	}
 	return nil
